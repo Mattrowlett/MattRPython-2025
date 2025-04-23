86e5e81ecfa5d743cdf6bc7ded0159ed09da7420
Mattrowlett <ma608480@ucf.edu>
2025-04-22 20:33:12 -0400

    Assignment5

--- assignment_05/demo_13_Classification/python_logistic_regression.py
+++ assignment_05/demo_13_Classification/python_logistic_regression.py
@@ -60,7 +60,7 @@ sns.set(style="whitegrid", color_codes=True)
 # Find out the current directory.
 os.getcwd()
 # Change to a new directory.
-git_path = 'C:\\Users\\le279259\\OneDrive - University of Central Florida\\Documents\\GitHub\\QMB3311S22\\'
+git_path = 'C:\\Users\\Matt Rowlett\\OneDrive - University of Central Florida\\Desktop\\GitHub QMB6315 Python\\MattRPython-2025\\assignment_05\\demo_13_Classification'
 os.chdir(git_path + 'demo_19_Classification')
 # Check that the change was successful.
 os.getcwd()
